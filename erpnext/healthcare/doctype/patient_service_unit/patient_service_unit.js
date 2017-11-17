de76d651a0639bd037352fb383edf261b288260b
jamsheer <kuttypjamsheer@gmail.com>
2017-11-17 16:22:51 +0530

    Patient Service Unit - New Doctype

---
+++ erpnext/healthcare/doctype/patient_service_unit/patient_service_unit.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, earthians and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Patient Service Unit', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ erpnext/healthcare/doctype/patient_service_unit/patient_service_unit.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017, earthians and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+
+class PatientServiceUnit(Document):
+	pass
+# 	def validate(self):
+# 		for child in self.assigned_users:
+# 			if child.user :
+# 				validate_for_enabled_user(child.user)
+# 				#validate_duplicate_user(child.user, self.name, child.name)
+#
+# 	def on_update(self):
+# 		for child in self.assigned_users:
+# 			if child.user :
+# 				frappe.permissions.add_user_permission("Patient Service Unit", self.name, child.user)
+#
+# def validate_for_enabled_user(user):
+# 	enabled = frappe.db.get_value("User", user, "enabled")
+# 	if enabled is None:
+# 		frappe.throw(_("User {0} does not exist").format(user))
+# 	if enabled == 0:
+# 		frappe.throw(_("User {0} is disabled").format(user))
+#
+# def validate_duplicate_user(user, parent):
+# 	users = frappe.db.sql_list("""select name from `tabUser List` where
+# 		user=%s and parent=%s""", (user, parent))
+# 	if users:
+# 		frappe.throw(_("Patient Service Unit {0} is already assigned to User {1}").format(parent, user), frappe.DuplicateEntryError)
