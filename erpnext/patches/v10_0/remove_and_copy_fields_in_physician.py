4078dfc54c6ad4ed22c5498d765c827e736db310
jamsheer <kuttypjamsheer@gmail.com>
2018-01-02 12:44:16 +0530

    Patches - fixed indentation

--- erpnext/patches/v10_0/remove_and_copy_fields_in_physician.py
+++ erpnext/patches/v10_0/remove_and_copy_fields_in_physician.py
@@ -1,20 +1,14 @@
 import frappe
 
 def execute():
-    #   If exist Physician
-
     if frappe.db.exists("DocType", "Physician"):
-
-		if 'physician_schedule' in frappe.db.get_table_columns("Physician"):
+        if 'physician_schedule' in frappe.db.get_table_columns("Physician"):
             for doc in frappe.get_all('Physician'):
                 _doc = frappe.get_doc('Physician', doc.name)
                 if _doc.physician_schedule:
                     _doc.append('physician_schedules', {'schedule': _doc.physician_schedule})
                     _doc.save()
+                    frappe.db.sql("alter table `tabPhysician` drop column physician_schedule")
 
-            #   Remove coloumn physician_schedule (Link - Physician Schedule DocType)
-			frappe.db.sql("alter table `tabPhysician` drop column physician_schedule")
-
-        #   Remove coloumn time_per_appointment (Time)
         if 'time_per_appointment' in frappe.db.get_table_columns("Physician"):
             frappe.db.sql("alter table `tabPhysician` drop coloumn time_per_appointment")
